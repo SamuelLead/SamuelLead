cb1226f61553cc303f44a7cfb5a924139827eb19
SamuelLead <92152008+SamuelLead@users.noreply.github.com>
2021-10-08 04:52:01 -0400

    Add files via upload

---
+++ SamFirst.java
@@ -0,0 +1,18 @@
+package javassignments;
+
+
+public class SamFirst {
+
+	public static void main(String[] args) {
+int number=-40;
+int sum;
+if(number<0)
+{
+sum=number*-1;
+System.out.println("the number " +number +" is converted to "+sum);
+
+		
+}
+}
+	}
+
