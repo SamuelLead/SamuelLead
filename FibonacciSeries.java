4983be4cc5877824cbc1e73f4fbf6b2f81e11c7b
SamuelLead <92152008+SamuelLead@users.noreply.github.com>
2021-10-08 05:06:04 -0400

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,16 @@
+package javassignments;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		int range= 8, firstnum = 0, secnum = 1, sum;
+		System.out.println(firstnum);
+		System.out.println(secnum);
+		for (int i =1; i <range; i++) {
+			sum= firstnum+secnum;
+			firstnum=secnum;
+			secnum=sum;
+			System.out.println(sum);
+		}
+	}
+
+}
