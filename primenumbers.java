c72fb9f9cd975d6396791f3837155032b98b35e1
SamuelLead <92152008+SamuelLead@users.noreply.github.com>
2021-10-08 08:00:27 -0400

    Add files via upload

---
+++ primenumbers.java
@@ -0,0 +1,23 @@
+package javassignments;
+
+import java.util.Iterator;
+
+public class primenumbers {
+	public static void main(String[] args) {
+		int input=13;
+		boolean flag=false;
+		for (int i = 2; i <input/2; i++) {
+			if (input%i==0) {
+				flag=true;
+				break;
+			
+			}
+		}
+		if (!flag)
+			System.out.println( input+ "is a prime number");
+		else
+			System.out.println(input+ "is not a prime number");
+	}
+		
+	}
+
