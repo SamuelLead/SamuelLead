102fc89074adda040d50ff2c9968b152db808b70
SamuelLead <92152008+SamuelLead@users.noreply.github.com>
2021-10-08 04:51:31 -0400

    Add files via upload

---
+++ SamFirstPro.java
@@ -0,0 +1,16 @@
+package javassignments;
+
+public class SamFirstPro {
+	
+public static void main(String[] args) {
+int num=0;
+if(num>0) {
+	System.out.println("number is positive");
+}
+	else if(num<0)
+	System.out.println("number is negative");
+	else
+		System.out.println("number is neither positive nor negative");
+}}
+
+
