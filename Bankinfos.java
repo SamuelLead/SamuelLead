50a739502c91ed054da24021d90664b34c158909
SamuelLead <92152008+SamuelLead@users.noreply.github.com>
2021-11-06 04:44:23 -0400

    Add files via upload

---
+++ Bankinfos.java
@@ -0,0 +1,16 @@
+
+public class Bankinfos {
+
+		public void saving() {
+		System.out.println("savings account interest rate is 2%");
+		}
+		public void fixed() {
+		System.out.println("fixed account interest is 7%");
+
+		}
+		public void deposit() {
+		System.out.println("deposit is 20000");
+		}
+		
+}
+
